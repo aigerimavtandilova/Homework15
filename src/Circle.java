503b8cbe6e1eded03c32b6e43b6a9c79d5d55d89
Aigerim <aygerim083@gmail.com>
2022-11-12 21:42:10 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,8 @@
+public class Circle {
+    public static void area(double radius){
+        System.out.println(Math.PI * (radius * radius));
+    }
+    public static void circumference(double radius){
+        System.out.println(Math.PI * 2 * radius);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Circle.area(5);
+    Circle.circumference(3);
+    }
+}
